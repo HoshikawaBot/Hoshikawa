ae16c16b7bfed1fe58985d449cd0348a040bab43
jiho lee <jiholee@kpu.ac.kr>
2020-02-22 08:36:20 +0900

    .

--- src/package/dcinside.py
+++ src/package/dcinside.py
@@ -83,7 +83,23 @@ def searchParse(author):
                         search_pos += 10000
                         page = 1
             except:
-                continue
+                try:
+                    botpagebox = soup.find("div", class_="bottom_paging_box")
+                    pages = [safe_int(a.text) for a in botpagebox.find_all("a")]
+                    maxPage = max(pages)
+                    if maxPage > page:
+                        page += 1
+                        continue
+
+                    if search_pos == 0:
+                        href = soup.find("a", class_="search_next")["href"]
+                        search_pos = int(re.compile(r"search_pos=(-\d*)").search(href).group(1))
+                        page = 1
+                    else:
+                        search_pos += 10000
+                        page = 1
+                except:
+                    continue
             
         if newPosts:
             result.update({gallId: newPosts[::-1]})
